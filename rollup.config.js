3bce6ae8680b9d0a671bab266ac374ec778d4729
Joseph Spurrier <github@josephspurrier.com>
2020-09-27 20:27:10 -0400

    Switch uglify for terser

--- rollup.config.js
+++ rollup.config.js
@@ -1,41 +1,10 @@
-/* eslint-disable @typescript-eslint/ban-ts-comment */
 import { nodeResolve } from '@rollup/plugin-node-resolve';
 import typescript from '@rollup/plugin-typescript';
-// @ts-ignore
-import { uglify } from 'rollup-plugin-uglify';
+import { terser } from 'rollup-plugin-terser';
 
 export default {
   input: 'src/lib/index.ts',
   output: [
-    // {
-    //   file: 'dist/index.browser.min.js',
-    //   format: 'iife',
-    //   name: 'mantium',
-    //   plugins: [
-    //     // eslint-disable-next-line @typescript-eslint/no-unsafe-call
-    //     uglify(),
-    //   ],
-    // },
-    // {
-    //   file: 'dist/index.browser.js',
-    //   format: 'iife',
-    //   name: 'mantium',
-    // },
-    // {
-    //   file: 'dist/index.cjs.js',
-    //   format: 'cjs',
-    //   name: 'mantium',
-    // },
-    // {
-    //   file: 'dist/index.system.js',
-    //   format: 'system',
-    //   name: 'mantium',
-    // },
-    // {
-    //   file: 'dist/index.amd.js',
-    //   format: 'amd',
-    //   name: 'mantium',
-    // },
     {
       file: 'dist/index.umd.js',
       format: 'umd',
@@ -46,15 +15,13 @@ export default {
       format: 'umd',
       name: 'mantium',
       plugins: [
-        // eslint-disable-next-line @typescript-eslint/no-unsafe-call
-        uglify(),
+        terser({
+          format: {
+            comments: false,
+          },
+        }),
       ],
     },
-    // {
-    //   file: 'dist/index.esm.js',
-    //   format: 'es',
-    //   name: 'mantium',
-    // },
   ],
   plugins: [
     typescript({
