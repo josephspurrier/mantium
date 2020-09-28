359c0e26ab17ee4ead3111fad0024f7887242bab
Joseph Spurrier <github@josephspurrier.com>
2020-09-27 21:03:19 -0400

    Use relative paths in package.json

--- src/test/fragment.test.tsx
+++ src/test/fragment.test.tsx
@@ -1,8 +1,10 @@
 import { m } from '../lib';
 import { cleanState } from '../lib/fragment';
 
+const h = m.createElement;
+
 test('set attribute class', () => {
-  const vn = m.createElement('div');
+  const vn = h('div');
   cleanState(vn);
   expect(vn).toStrictEqual({ attrs: {}, children: [], tag: 'div' });
 });
