413996e9e91408ec54017afbd12f88f0509c6f26
Joseph Spurrier <github@josephspurrier.com>
2020-09-29 00:03:09 -0400

    Clean up vnode test

--- src/test/vnode.test.tsx
+++ src/test/vnode.test.tsx
@@ -1,19 +1,7 @@
 import { m } from '../lib';
-//import { appendChildToNode } from '../lib/vnode';
-
-const h = m.createElement;
 
 test('vnode with children array', () => {
   const vn = m.createElement('div', {}, ['foo', 'bar']);
   m.render(document.body, () => vn);
   expect(document.body.outerHTML).toBe('<body><div>foobar</div></body>');
 });
-
-// test('vnode with fragment', () => {
-//   //const vn = createDocFragment('foo');
-//   appendChildToNode(document.body, h('div', {}, 'thanks'));
-//   //m.render(document.body, () => vn);
-//   expect(document.body.outerHTML).toBe(
-//     '<body><div>foobar</div><div>thanks</div></body>',
-//   );
-// });
