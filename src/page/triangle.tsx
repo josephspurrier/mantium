f60a821a993dabb018b3b28ea43d00a2075aa9cb
Joseph Spurrier <github@josephspurrier.com>
2020-10-14 23:02:25 -0400

    Clean up the triangle file

--- src/page/triangle.tsx
+++ src/page/triangle.tsx
@@ -1,23 +1,9 @@
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
 import { m } from '../lib';
 import { useEffect } from '../lib/useeffect';
-//import { createSignal, onCleanup, createMemo, createDeferred } from 'solid-js';
 import { useState } from '../lib/usestate';
 
 const TARGET = 25;
-
-// export const TriangleDemo = (): JSX.Element => {
-//   return (
-//     <div
-//       class='container'
-//       style={{
-//         transform: `scaleX(${4.2 / 2.1}) scaleY(0.7) translateZ(0.1px)`,
-//       }}
-//     >
-//       Test
-//     </div>
-//   );
-// };
 
 export const TriangleDemo = (): JSX.Element => {
   const [elapsed, setElapsed] = useState(0);
@@ -29,11 +15,6 @@ export const TriangleDemo = (): JSX.Element => {
     return 1 + (e > 5 ? 10 - e : e) / 10;
   };
 
-  //const start = Date.now();
-  //console.log('Now', start);
-
-  let f: number;
-
   useEffect(() => {
     const t = setInterval(() => {
       setSeconds((prev) => (prev % 10) + 1);
@@ -41,18 +22,13 @@ export const TriangleDemo = (): JSX.Element => {
 
     const update = () => {
       setElapsed(Date.now() - start);
-      //console.log('here');
       f = requestAnimationFrame(update);
     };
-    f = requestAnimationFrame(update);
-    //console.log('add');
+    let f = requestAnimationFrame(update);
     return () => {
-      //console.log('remove');
       clearInterval(t), cancelAnimationFrame(f);
     };
   }, []);
-
-  //console.log('Scale:', scale());
 
   return (
     <div>
@@ -80,21 +56,14 @@ interface Attrs {
 const Triangle = ({ x, y, s, seconds }: Attrs) => {
   if (s <= TARGET) {
     return (
-      <Dot x={x - TARGET / 2} y={y - TARGET / 2} s={TARGET} text={seconds} />
+      <Dot x={x - TARGET / 2} y={y - TARGET / 2} s={TARGET} seconds={seconds} />
     );
   }
   s = s / 2;
 
-  //console.log('s:', s);
-
-  if (s === 62.5) {
-    //console.log('boom');
-    //seconds; //seconds = createDeferred(seconds);
-  }
-
-  // var slowDown = true;
+  // const slowDown = true;
   // if (slowDown) {
-  //   var e = performance.now() + 0.8;
+  //   const e = performance.now() + 0.8;
   //   while (performance.now() < e) {
   //     // Artificially long execution time.
   //   }
@@ -109,18 +78,10 @@ const Triangle = ({ x, y, s, seconds }: Attrs) => {
   );
 };
 
-interface Attrs2 {
-  x: number;
-  y: number;
-  s: number;
-  text: number;
-}
-
-const Dot = ({ x, y, s, text }: Attrs2) => {
+const Dot = ({ x, y, s, seconds }: Attrs) => {
   const [hover, setHover] = useState(false),
     onEnter = () => setHover(true),
     onExit = () => setHover(false);
-  //console.log(x, y);
   return (
     <div
       class='dot'
@@ -136,7 +97,7 @@ const Dot = ({ x, y, s, text }: Attrs2) => {
       onMouseEnter={onEnter}
       onMouseLeave={onExit}
     >
-      {hover ? `**${text}**` : text}
+      {hover ? `**${seconds}**` : seconds}
     </div>
   );
 };
