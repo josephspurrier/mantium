851fb12a957cdedf017746111455e9e492123ddb
Joseph Spurrier <github@josephspurrier.com>
2020-10-19 23:17:36 -0400

    Clean up the commit function

--- src/lib/v2/commit.ts
+++ src/lib/v2/commit.ts
@@ -14,7 +14,6 @@ export function render(
   container: HTMLElement,
 ): void {
   setWorkDone(false);
-
   //console.log('Render:', rawElement, container);
 
   let elementType: NodeType;
@@ -67,7 +66,6 @@ export function setRootParent(element: HTMLElement): void {
 export function redraw(origin = ''): void {
   //console.log('redraw:', origin);
 
-  // TODO: I added this on currentRoot.
   if (currentRoot) {
     wipRoot = {
       type: currentRoot.type,
@@ -86,23 +84,10 @@ export function commitRoot(deletes: Fiber[], wip: Fiber): void {
   if (verbose) console.log('CommitRoot:', wip);
   if (verbose) console.log('Deletions:', deletes);
 
-  const stop = deletes.length > 0;
-
   // Process each deletion, but don't process siblings.
   deletes.forEach((fiber: Fiber) => {
-    // FIXME: Checking for dom is not correct because it could be a function
-    // and they don't have a DOM.
-    if (fiber.dom) {
-      commitWork(fiber, false);
-    } else {
-      //console.log('Found fragment on delete', fiber);
-      commitWork(fiber, false);
-    }
+    commitWork(fiber);
   });
-
-  if (stop) {
-    //throw new Error('stopped!');
-  }
 
   if (wip) {
     commitWork(wip.child, false);
@@ -111,85 +96,53 @@ export function commitRoot(deletes: Fiber[], wip: Fiber): void {
   wipRoot = undefined;
 }
 
-function commitWork(fiber: Fiber | undefined, sibling: boolean) {
-  if (!fiber) {
+function commitWork(fiber: Fiber | undefined, sibling = false) {
+  if (!fiber || !fiber.parent) {
     return;
   }
-
-  let domParentFiber = fiber.parent;
 
   // For the fiber that was passed (and it could be a deletion), get the parent
   // and if the parent doesn't have a dom, then get the grandparent, and
   // continue upwards until a dom is found.
-  if (domParentFiber) {
-    while (domParentFiber && !domParentFiber.dom) {
-      domParentFiber = domParentFiber.parent;
-    }
-
-    // Once a parent is found with a dom, check the fiber tag to see what
-    // operation needs to be handled on it.
-    const domParent = domParentFiber && domParentFiber.dom;
-    if (domParent) {
-      if (fiber.effectTag === 'PLACEMENT' && fiber.dom) {
-        if (domParent) {
-          //console.log('PLACER:', domParent, typeof domParent, fiber);
-
-          // if (
-          //   fiber.index > -1 &&
-          //   domParent.childNodes.length > 0 &&
-          //   domParent.childNodes[fiber.index] //FIXME: This will be null and then it will do an appendChild anyway.
-          // ) {
-          //   //console.log('BAD!', domParent.childNodes[fiber.index]);
-          //   domParent.insertBefore(
-          //     fiber.dom,
-          //     domParent.childNodes[fiber.index],
-          //   );
-          // } else {
-          domParent.appendChild(fiber.dom);
-          //}
-
-          //domParent.insertBefore();
-        } else {
-          console.log('MISSING PARENT!');
-        }
-      } else if (fiber.effectTag === 'UPDATE' && fiber.dom) {
-        if (fiber.alternate) {
-          updateDom(fiber.dom, fiber.alternate.props, fiber.props);
-
-          //fiber.dom.textContent = 'cool';
-          //console.log('UPDATED:', fiber);
-          // FIXME: This appendChild should not be here!!! Maybe the parent was removed earlier? The node should still be in the DOM.
-          // if (domParent.childNodes.length > 1) {
-          //domParent.appendChild(fiber.dom);
-          // }
-          if (
-            domParent.lastChild &&
-            !domParent.lastChild.isSameNode(fiber.dom)
-          ) {
-            // console.log(
-            //   'Not last:',
-            //   (domParent.lastChild as HTMLElement).outerHTML,
-            //   (fiber.dom as HTMLElement).outerHTML,
-            // );
-            //domParent.appendChild(fiber.dom);
-          }
-        } else {
-          console.log('MISSING ALTERNATVE!');
-        }
-      } else if (fiber.effectTag === 'DELETION') {
-        if (domParent) {
-          commitDeletion(fiber, domParent, sibling);
-        } else {
-          console.log('MISSING DOMPARENT!');
-        }
-        return;
-      } else {
-        //console.log('NOTHING!', fiber);
-      }
-      commitWork(fiber.child, false);
-      commitWork(fiber.sibling, false);
-    }
+  let domParentFiber: Fiber | undefined = fiber.parent;
+  while (domParentFiber && !domParentFiber.dom) {
+    domParentFiber = domParentFiber.parent;
   }
+
+  // Once a parent is found with a dom, check the fiber tag to see what
+  // operation needs to be handled on it.
+  const domParent = domParentFiber && domParentFiber.dom;
+  if (!domParent) {
+    return;
+  }
+
+  if (fiber.effectTag === 'PLACEMENT' && fiber.dom) {
+    domParent.appendChild(fiber.dom);
+  } else if (fiber.effectTag === 'UPDATE' && fiber.dom) {
+    if (!fiber.alternate) {
+      console.log('MISSING ALTERNATVE!');
+      return;
+    }
+
+    updateDom(fiber.dom, fiber.alternate.props, fiber.props);
+    // if (domParent.lastChild && !domParent.lastChild.isSameNode(fiber.dom)) {
+    //   // console.log(
+    //   //   'Not last:',
+    //   //   (domParent.lastChild as HTMLElement).outerHTML,
+    //   //   (fiber.dom as HTMLElement).outerHTML,
+    //   // );
+    // }
+
+    domParent.appendChild(fiber.dom);
+  } else if (fiber.effectTag === 'DELETION') {
+    commitDeletion(fiber, domParent, sibling);
+    return;
+  } else {
+    //console.log('NOTHING!', fiber);
+  }
+
+  commitWork(fiber.child);
+  commitWork(fiber.sibling);
 
   return;
 }
